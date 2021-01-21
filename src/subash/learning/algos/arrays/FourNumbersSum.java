b72c5ab82ff94fa07a1305e1ed08c9365696f635
subash kharel <subash-minu@localhost.attlocal.net>
2021-01-21 15:13:42 -0600

    bad but working example for fourNumberSum alog

---
+++ src/subash/learning/algos/arrays/FourNumbersSum.java
@@ -0,0 +1,34 @@
+package subash.learning.algos.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourNumbersSum {
+
+
+    // O(n^4) time complexity
+    //O(1) space complexity
+    public static List<Integer[]> fourNumberSum(int[] array, int target){
+        List<Integer[]> arrayOfInteger = new ArrayList<>();
+        for(int i =0; i<array.length-3;i++){
+            for(int j= i+1; j<array.length-2;j++){
+                for(int k =j+1; k<array.length-1;k++){
+                    for(int l = k+1; l<array.length;l++){
+                        if((array[i]+array[j]+array[k]+array[l]) == target){
+                            Integer[] matchedArr = {array[i],array[j],array[k],array[l]};
+                            arrayOfInteger.add(matchedArr);
+                        }
+                    }
+                }
+            }
+        }
+        return arrayOfInteger;
+    }
+
+    public static void main(String[] args) {
+          int[] testArray = new int[]{7, 6, 4, -1, 1, 2};
+        int targetSum = 16;
+      System.out.println(" Method---->" + Arrays.deepToString((fourNumberSum(testArray,targetSum).toArray())));
+    }
+}
